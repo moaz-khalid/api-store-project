6938c2da291225e079d96b70ae9e016038709ac4
moaz-khalid <moazkhalid055@gmail.com>
2026-09-22 22:47:57 +0300

    added tokens

---
+++ Store.Core/Services.Contract/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Core.Services.Contract
+{
+    internal interface ITokenService
+    {
+    }
+}
---
+++ Store.Service/Services/Tokens/TokenService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Service.Services.Tokens
+{
+    internal class TokenService
+    {
+    }
+}
